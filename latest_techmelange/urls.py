af0c69568d84a289f8b7fa776387243eeb30e3df
Vaishnavi Mall <vaishnavi.mall@startxlabs.com>
2022-03-02 00:36:08 +0530

    third

--- latest_techmelange/urls.py
+++ latest_techmelange/urls.py
@@ -4,8 +4,6 @@ from django.urls import path,include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.conf.urls import url
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('myapp.urls')),
