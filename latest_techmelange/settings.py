f1c6bf5e38a4b3f3d6938688bcb7f8febcee6d3b
Vaishnavi Mall <vaishnavi.mall@startxlabs.com>
2022-03-02 00:12:16 +0530

    third

--- latest_techmelange/settings.py
+++ latest_techmelange/settings.py
@@ -126,10 +126,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-#added manually 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #Media 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
